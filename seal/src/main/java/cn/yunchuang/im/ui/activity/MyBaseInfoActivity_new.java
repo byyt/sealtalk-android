3f975381fda73d72ebe14715260d515a2fa4e4e1
zhouyuntao <1067662561@qq.com>
2019-03-23 15:46:48 +0800

    个人信息编辑，性别、出生日期、身高、所在地

--- seal/src/main/java/cn/yunchuang/im/ui/activity/MyBaseInfoActivity_new.java
+++ seal/src/main/java/cn/yunchuang/im/ui/activity/MyBaseInfoActivity_new.java
@@ -1,41 +1,188 @@
 package cn.yunchuang.im.ui.activity;
 
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v4.widget.NestedScrollView;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.itheima.roundedimageview.RoundedImageView;
+import com.jrmf360.rylib.common.util.ToastUtil;
+import com.mylhyl.circledialog.CircleDialog;
+import com.mylhyl.circledialog.callback.ConfigDialog;
+import com.mylhyl.circledialog.callback.ConfigSubTitle;
+import com.mylhyl.circledialog.callback.ConfigTitle;
+import com.mylhyl.circledialog.params.DialogParams;
+import com.mylhyl.circledialog.params.SubTitleParams;
+import com.mylhyl.circledialog.params.TitleParams;
+
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
+import cn.qqtheme.framework.entity.City;
+import cn.qqtheme.framework.entity.County;
+import cn.qqtheme.framework.entity.Province;
 import cn.qqtheme.framework.picker.DatePicker;
+import cn.qqtheme.framework.picker.OptionPicker;
+import cn.qqtheme.framework.picker.WheelPicker;
 import cn.qqtheme.framework.util.ConvertUtils;
+import cn.qqtheme.framework.widget.WheelView;
 import cn.yunchuang.im.R;
+import cn.yunchuang.im.server.BaseAction;
 import cn.yunchuang.im.server.network.http.HttpException;
+import cn.yunchuang.im.widget.AddressPickTask;
+import cn.yunchuang.im.zmico.statusbar.StatusBarCompat;
+import cn.yunchuang.im.zmico.utils.BaseBaseUtils;
+import cn.yunchuang.im.zmico.utils.DeviceUtils;
 import cn.yunchuang.im.zmico.utils.ResourceUtils;
 
 
-public class MyBaseInfoActivity_new extends BaseActivity implements View.OnClickListener {
+public class MyBaseInfoActivity_new extends BaseActivity implements View.OnClickListener,
+        View.OnLongClickListener {
 
-    private TextView birthDayTv;
-    private int setYear = 1992;
-    private int setMonth = 2;
-    private int setDay = 16;
+    private NestedScrollView nestedScrollView;
+
+    private FrameLayout titleLayout;
+    private ImageView backImg;
+    private TextView saveTv;
+
+    private LinearLayout zhaopianLayout;
+    private LinearLayout nichengLayout;
+    private TextView nichengTv;
+    private LinearLayout xingbieLayout;
+    private TextView xingbieTv;
+    private LinearLayout shengriLayout;
+    private TextView shengriTv;
+    private LinearLayout shengaoLayout;
+    private TextView shengaoTv;
+    private LinearLayout szdLayout;
+    private TextView szdTv;
+
+    private int screenWidth;
+    private int zhaopianWidth;
+
+    private String setSex = "男";
+    private String setYear = "1990";
+    private String setMonth = "01";
+    private String setDay = "01";
+    private String setShengao = "180cm";
+    private String setProvince = "广东省";
+    private String setCity = "深圳市";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StatusBarCompat.setStatusBarColor(this, Color.WHITE);
+        BaseBaseUtils.setTranslucentStatus(this);//状态栏透明
         setContentView(R.layout.activity_my_base_info);
         setTitle(R.string.de_actionbar_myacc);
+        setHeadVisibility(View.GONE);
 
         initView();
     }
 
     private void initView() {
-        findViewById(R.id.my_base_info_shengri_layout).setOnClickListener(this);
-        birthDayTv = (TextView) findViewById(R.id.my_base_info_shengri_tv);
+        screenWidth = DeviceUtils.getScreenWidthPixels(this);
+        zhaopianWidth = (screenWidth - DeviceUtils.dpToPx(35) - (DeviceUtils.dpToPx(5) * 3)) / 4;
+
+        nestedScrollView = (NestedScrollView) findViewById(R.id.activity_base_info_root_scrollview);
+
+        titleLayout = (FrameLayout) findViewById(R.id.activity_base_info_title_layout);
+        backImg = (ImageView) findViewById(R.id.activity_base_info_back);
+        backImg.setOnClickListener(this);
+        saveTv = (TextView) findViewById(R.id.activity_base_info_save);
+        saveTv.setOnClickListener(this);
+
+        zhaopianLayout = (LinearLayout) findViewById(R.id.activity_base_info_zhaopian_layout);
+        nichengLayout = (LinearLayout) findViewById(R.id.activity_base_info_nicheng_layout);
+        nichengLayout.setOnClickListener(this);
+        nichengTv = (TextView) findViewById(R.id.activity_base_info_nicheng_tv);
+        xingbieLayout = (LinearLayout) findViewById(R.id.activity_base_info_xingbie_layout);
+        xingbieLayout.setOnClickListener(this);
+        xingbieTv = (TextView) findViewById(R.id.activity_base_info_xingbie_tv);
+        shengriLayout = (LinearLayout) findViewById(R.id.activity_base_info_shengri_layout);
+        shengriLayout.setOnClickListener(this);
+        shengriTv = (TextView) findViewById(R.id.activity_base_info_shengri_tv);
+        shengaoLayout = (LinearLayout) findViewById(R.id.activity_base_info_shengao_layout);
+        shengaoLayout.setOnClickListener(this);
+        shengaoTv = (TextView) findViewById(R.id.activity_base_info_shengao_tv);
+        szdLayout = (LinearLayout) findViewById(R.id.activity_base_info_szd_layout);
+        szdLayout.setOnClickListener(this);
+        szdTv = (TextView) findViewById(R.id.activity_base_info_szd_tv);
+
+        initTitleLayout();
+        List<String> imgList = new ArrayList<>();
+        for (int i = 16; i < 26; i++) {
+            imgList.add(BaseAction.DOMAIN_PIC + "/" + i + ".jpg");
+        }
+        initZhaoPianLayout(imgList);
+
+        nestedScrollView.scrollTo(0, 0);
     }
 
+    private void initTitleLayout() {
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                DeviceUtils.dpToPx(48) + DeviceUtils.getStatusBarHeightPixels(this));
+        titleLayout.setLayoutParams(layoutParams);
+    }
+
+    public void initZhaoPianLayout(List<String> zhaopianList) {
+        zhaopianLayout.removeAllViews();
+        zhaopianLayout.addView(createAddItem());
+        for (int i = 0; i < zhaopianList.size(); i++) {
+            View view;
+            String imgUrl = zhaopianList.get(i);
+            view = createImgItem(imgUrl);
+            view.setTag(imgUrl);
+            zhaopianLayout.addView(view);
+        }
+    }
+
+    private View createAddItem() {
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_my_base_info_zhaopian_add, zhaopianLayout, false);
+        ImageView addIv = (ImageView) view.findViewById(R.id.activity_base_info_add_zhaopin_iv);
+        //计算宽度，使正好能显示四个图片（包含添加）
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(zhaopianWidth, zhaopianWidth);
+        params.setMargins(0, 0, DeviceUtils.dpToPx(5), 0);
+        addIv.setLayoutParams(params);
+        addIv.setOnClickListener(this);
+        return view;
+    }
+
+    private View createImgItem(String imgUrl) {
+        View view = LayoutInflater.from(this).inflate(R.layout.layout_my_base_info_zhaopian_item, zhaopianLayout, false);
+        RoundedImageView itemIv = (RoundedImageView) view.findViewById(R.id.activity_base_info_item_zhaopin_iv);
+        FrameLayout.LayoutParams params = new FrameLayout.LayoutParams(zhaopianWidth, zhaopianWidth);
+        params.setMargins(0, 0, DeviceUtils.dpToPx(5), 0);
+        itemIv.setLayoutParams(params);
+        itemIv.setOnClickListener(this);
+        itemIv.setOnLongClickListener(this);
+
+        RequestOptions options = new RequestOptions();//正常加载
+
+        options.placeholder(R.drawable.ic_image_zhanwei)
+                .error(R.drawable.ic_image_zhanwei);
+
+        Glide.with(this)
+                .load(imgUrl)
+                .apply(options)
+                .into(itemIv);
+
+        return view;
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -71,45 +218,117 @@ public class MyBaseInfoActivity_new extends BaseActivity implements View.OnClick
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.my_base_info_shengri_layout:
-                onYearMonthDayTimePicker();
+            case R.id.activity_base_info_back:
+                finish();
+            case R.id.activity_base_info_add_zhaopin_iv:
+                showSelectPictureDialog();
+            case R.id.activity_base_info_item_zhaopin_iv:
+                showSelectPictureDialog();
+            case R.id.activity_base_info_xingbie_layout:
+                onXingbiePicker();
+                break;
+            case R.id.activity_base_info_shengri_layout:
+                onShengriPicker();
+                break;
+            case R.id.activity_base_info_shengao_layout:
+                onShengaoPicker();
+                break;
+            case R.id.activity_base_info_szd_layout:
+                onSzdPicker();
                 break;
         }
     }
 
-    private void onYearMonthDayTimePicker() {
+    @Override
+    public boolean onLongClick(View view) {
+        switch (view.getId()) {
+            case R.id.activity_base_info_item_zhaopin_iv:
+                showSelectPictureDialog();
+                break;
+        }
+        return true;
+    }
+
+    private void showSelectPictureDialog() {
+        final List<PictureTypeEntity> items = new ArrayList<>();
+        items.add(new PictureTypeEntity(1, "拍照"));
+        items.add(new PictureTypeEntity(2, "从相册选择"));
+        items.add(new PictureTypeEntity(3, "小视频"));
+//                final String[] items = {"拍照", "从相册选择", "小视频"};
+        new CircleDialog.Builder()
+                .configDialog(new ConfigDialog() {
+                    @Override
+                    public void onConfig(DialogParams params) {
+                        params.backgroundColorPress = Color.CYAN;
+                        //增加弹出动画
+                        params.animStyle = R.style.MDBottomDialogStyle_AnimationStyle;
+//                            params.gravity = Gravity.TOP;
+                    }
+                })
+                .setTitle("标题")
+//                        .setTitleColor(Color.BLUE)
+                .configTitle(new ConfigTitle() {
+                    @Override
+                    public void onConfig(TitleParams params) {
+                        //                                params.backgroundColor = Color.RED;
+                    }
+                })
+                .setSubTitle("副标题：请从以下中选择照片的方式进行提交")
+                .configSubTitle(new ConfigSubTitle() {
+                    @Override
+                    public void onConfig(SubTitleParams params) {
+                        //                                params.backgroundColor = Color.YELLOW;
+                    }
+                })
+                .setItems(items, new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                        Toast.makeText(MyBaseInfoActivity_new.this, "点击了：" + items.get(i)
+                                , Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegative("取消", null)
+//                        .setNeutral("中间", null)
+//                        .setPositive("确定", null)
+//                        .configNegative(new ConfigButton() {
+//                            @Override
+//                            public void onConfig(ButtonParams params) {
+//                                //取消按钮字体颜色
+//                                params.textColor = Color.RED;
+//                                params.backgroundColorPress = Color.BLUE;
+//                            }
+//                        })
+                .show(getSupportFragmentManager());
+    }
+
+    private void onXingbiePicker() {
+        OptionPicker picker = new OptionPicker(this, new String[]{"男", "女"});
+        setPickerStyle(picker);
+        picker.setTitleText("性别");
+        picker.setOnOptionPickListener(new OptionPicker.OnOptionPickListener() {
+            @Override
+            public void onOptionPicked(int index, String item) {
+                setSex = item;
+                xingbieTv.setText(item);
+            }
+        });
+        picker.setSelectedItem(setSex);
+        picker.show();
+    }
+
+    private void onShengriPicker() {
         final DatePicker picker = new DatePicker(this);
-        picker.setCanceledOnTouchOutside(true);
-        picker.setUseWeight(true);
-        picker.setTopPadding(ConvertUtils.toPx(this, 10));
-
-        picker.setDividerColor(ResourceUtils.getColor(R.color.color_8E8E8E));
-        picker.setDividerRatio(0);
-        picker.setTextColor(ResourceUtils.getColor(R.color.black));
-        picker.setTextSize(20);
-        picker.setLabelTextColor(ResourceUtils.getColor(R.color.black));
-        picker.setCancelTextColor(ResourceUtils.getColor(R.color.black));
-        picker.setCancelTextSize(16);
-        picker.setSubmitTextColor(ResourceUtils.getColor(R.color.black));
-        picker.setSubmitTextSize(ResourceUtils.getColor(R.color.black));
-        picker.setPressedTextColor(ResourceUtils.getColor(R.color.color_888888));
+        setPickerStyle(picker);
         picker.setTitleText("出生日期");
-        picker.setTitleTextColor(ResourceUtils.getColor(R.color.black));
-        picker.setTitleTextSize(18);
-        picker.setTopLineColor(ResourceUtils.getColor(R.color.color_E4E4E4));
-        picker.setTopHeight(45);
-        picker.setHeight(ResourceUtils.dpToPX(260));
-
-        picker.setAnimationStyle(R.style.MDBottomDialogStyle_AnimationStyle);
-
+        picker.setContentPadding(20, 0);
         picker.setResetWhileWheel(false);
         picker.setOnDatePickListener(new DatePicker.OnYearMonthDayPickListener() {
             @Override
             public void onDatePicked(String year, String month, String day) {
-                birthDayTv.setText(year + "-" + month + "-" + day);
-                setYear = Integer.valueOf(year);
-                setMonth = Integer.valueOf(month);
-                setDay = Integer.valueOf(day);
+                shengriTv.setText(year + "-" + month + "-" + day);
+                setYear = year;
+                setMonth = month;
+                setDay = day;
             }
         });
 
@@ -122,8 +341,100 @@ public class MyBaseInfoActivity_new extends BaseActivity implements View.OnClick
         int startYear = currentYear - 70;
         picker.setRangeEnd(endYear, currentMonth, currentDay);
         picker.setRangeStart(startYear, currentMonth, currentDay);
-        picker.setSelectedItem(setYear, setMonth, setDay);
+        picker.setSelectedItem(Integer.valueOf(setYear), Integer.valueOf(setMonth), Integer.valueOf(setDay));
 
         picker.show();
     }
+
+    private void onShengaoPicker() {
+        OptionPicker picker = new OptionPicker(this, getShengaoStringArray());
+        setPickerStyle(picker);
+        picker.setTitleText("身高");
+        picker.setOnOptionPickListener(new OptionPicker.OnOptionPickListener() {
+            @Override
+            public void onOptionPicked(int index, String item) {
+                setShengao = item;
+                shengaoTv.setText(item);
+            }
+        });
+        picker.setSelectedItem(setShengao);
+        picker.show();
+    }
+
+    private void onSzdPicker() {
+        AddressPickTask task = new AddressPickTask(this);
+        task.setHideCounty(true);
+        task.setCallback(new AddressPickTask.Callback() {
+            @Override
+            public void onAddressInitFailed() {
+                ToastUtil.showToast(MyBaseInfoActivity_new.this, "数据初始化失败");
+            }
+
+            @Override
+            public void onAddressPicked(Province province, City city, County county) {
+//                showToast(province.getAreaName() + " " + city.getAreaName());
+                setProvince = province.getAreaName();
+                setCity = city.getAreaName();
+                szdTv.setText(province.getAreaName() + " " + city.getAreaName());
+            }
+        });
+        task.execute(setProvince, setCity);
+    }
+
+    private void setPickerStyle(WheelPicker picker) {
+        picker.setCanceledOnTouchOutside(true);
+        picker.setUseWeight(true);
+        picker.setTopPadding(ConvertUtils.toPx(this, 10));
+        picker.setDividerColor(ResourceUtils.getColor(R.color.color_E4E4E4));
+        picker.setDividerRatio(WheelView.DividerConfig.FILL);
+        picker.setTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setTextSize(20);
+        picker.setLabelTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setCancelTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setCancelTextSize(16);
+        picker.setSubmitTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setSubmitTextSize(ResourceUtils.getColor(R.color.black));
+        picker.setPressedTextColor(ResourceUtils.getColor(R.color.color_888888));
+        picker.setTitleTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setTitleTextSize(18);
+        picker.setTopLineColor(ResourceUtils.getColor(R.color.color_E4E4E4));
+        picker.setTopHeight(45);
+        picker.setHeight(ResourceUtils.dpToPX(260));
+        picker.setAnimationStyle(R.style.MDBottomDialogStyle_AnimationStyle);
+    }
+
+    private String[] getShengaoStringArray() {
+        return new String[]{"145cm", "146cm", "147cm", "148cm", "149cm", "150cm",
+                "151cm", "152cm", "153cm", "154cm", "155cm", "156cm", "157cm", "158cm", "159cm", "160cm",
+                "161cm", "162cm", "163cm", "164cm", "165cm", "166cm", "167cm", "168cm", "169cm", "170cm",
+                "171cm", "172cm", "173cm", "174cm", "175cm", "176cm", "177cm", "178cm", "179cm", "180cm",
+                "181cm", "182cm", "183cm", "184cm", "185cm", "186cm", "187cm", "188cm", "189cm", "190cm",
+                "191cm", "192cm", "193cm", "194cm", "195cm", "196cm", "197cm", "198cm", "199cm", "200cm"};
+    }
+
+    public class PictureTypeEntity
+//        implements CircleItemLabel
+    {
+        public int id;
+        public String typeName;
+
+        public PictureTypeEntity() {
+        }
+
+        public PictureTypeEntity(int id, String typeName) {
+            this.id = id;
+            this.typeName = typeName;
+        }
+
+        @Override
+        public String toString() {
+            return typeName;
+        }
+
+//    @Override
+//    public String getItemLabel() {
+//        return typeName;
+//    }
+    }
+
 }
---
+++ seal/src/main/java/cn/yunchuang/im/widget/AddressPickTask.java
@@ -0,0 +1,149 @@
+package cn.yunchuang.im.widget;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+
+import com.alibaba.fastjson.JSON;
+
+import java.lang.ref.WeakReference;
+import java.util.ArrayList;
+
+import cn.qqtheme.framework.entity.Province;
+import cn.qqtheme.framework.picker.AddressPicker;
+import cn.qqtheme.framework.picker.WheelPicker;
+import cn.qqtheme.framework.util.ConvertUtils;
+import cn.qqtheme.framework.widget.WheelView;
+import cn.yunchuang.im.App;
+import cn.yunchuang.im.R;
+import cn.yunchuang.im.zmico.utils.ResourceUtils;
+
+/**
+ * 获取地址数据并显示地址选择器
+ *
+ * @author 李玉江[QQ:1032694760]
+ * @since 2015/12/15
+ */
+public class AddressPickTask extends AsyncTask<String, Void, ArrayList<Province>> {
+    private WeakReference<Activity> activityReference;// 2018/6/1 StaticFieldLeak
+    private ProgressDialog dialog;
+    private Callback callback;
+    private String selectedProvince = "", selectedCity = "", selectedCounty = "";
+    private boolean hideProvince = false;
+    private boolean hideCounty = false;
+
+    public AddressPickTask(Activity activity) {
+        this.activityReference = new WeakReference<>(activity);
+    }
+
+    public void setHideProvince(boolean hideProvince) {
+        this.hideProvince = hideProvince;
+    }
+
+    public void setHideCounty(boolean hideCounty) {
+        this.hideCounty = hideCounty;
+    }
+
+    public void setCallback(Callback callback) {
+        this.callback = callback;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        Activity activity = activityReference.get();
+        if (activity == null) {
+            return;
+        }
+//        dialog = ProgressDialog.show(activity, null, "正在初始化数据...", true, true);
+    }
+
+    @Override
+    protected ArrayList<Province> doInBackground(String... params) {
+        if (params != null) {
+            switch (params.length) {
+                case 1:
+                    selectedProvince = params[0];
+                    break;
+                case 2:
+                    selectedProvince = params[0];
+                    selectedCity = params[1];
+                    break;
+                case 3:
+                    selectedProvince = params[0];
+                    selectedCity = params[1];
+                    selectedCounty = params[2];
+                    break;
+                default:
+                    break;
+            }
+        }
+        ArrayList<Province> data = new ArrayList<>();
+        try {
+            Activity activity = activityReference.get();
+            if (activity != null) {
+                String json = ConvertUtils.toString(activity.getAssets().open("city.json"));
+                data.addAll(JSON.parseArray(json, Province.class));
+            }
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    @Override
+    protected void onPostExecute(ArrayList<Province> result) {
+        if (dialog != null) {
+            dialog.dismiss();
+        }
+        if (result.size() > 0) {
+            Activity activity = activityReference.get();
+            if (activity == null) {
+                return;
+            }
+            AddressPicker picker = new AddressPicker(activity, result);
+            setPickerStyle(picker);
+            picker.setTitleText("所在地");
+            picker.setHideProvince(hideProvince);
+            picker.setHideCounty(hideCounty);
+            if (hideCounty) {
+                picker.setColumnWeight(1 / 3.0f, 2 / 3.0f);//将屏幕分为3份，省级和地级的比例为1:2
+            } else {
+                picker.setColumnWeight(2 / 8.0f, 3 / 8.0f, 3 / 8.0f);//省级、地级和县级的比例为2:3:3
+            }
+            picker.setSelectedItem(selectedProvince, selectedCity, selectedCounty);
+            picker.setOnAddressPickListener(callback);
+            picker.show();
+        } else {
+            callback.onAddressInitFailed();
+        }
+    }
+
+    public interface Callback extends AddressPicker.OnAddressPickListener {
+
+        void onAddressInitFailed();
+
+    }
+
+    private void setPickerStyle(WheelPicker picker) {
+        picker.setCanceledOnTouchOutside(true);
+        picker.setUseWeight(true);
+        picker.setTopPadding(ConvertUtils.toPx(App.getAppContext(), 10));
+        picker.setDividerColor(ResourceUtils.getColor(R.color.color_E4E4E4));
+        picker.setDividerRatio(WheelView.DividerConfig.FILL);
+        picker.setTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setTextSize(20);
+        picker.setLabelTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setCancelTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setCancelTextSize(16);
+        picker.setSubmitTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setSubmitTextSize(ResourceUtils.getColor(R.color.black));
+        picker.setPressedTextColor(ResourceUtils.getColor(R.color.color_888888));
+        picker.setTitleTextColor(ResourceUtils.getColor(R.color.black));
+        picker.setTitleTextSize(18);
+        picker.setTopLineColor(ResourceUtils.getColor(R.color.color_E4E4E4));
+        picker.setTopHeight(45);
+        picker.setHeight(ResourceUtils.dpToPX(260));
+        picker.setAnimationStyle(R.style.MDBottomDialogStyle_AnimationStyle);
+    }
+
+}
